cf6aaf965cb9e575fe2b910e94c5d9dbe4166a9c
blashak <wipcrep@gmail.com>
2014-11-14 18:36:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-//Default settings
+angular.module('app')
+  .controller('AppCtrl', ['$scope', 'googleMaps', function ($scope, googleMaps) {
+    
+    //Default settings
+    googleMaps.setPosition(currentCoordinates);
+    googleMaps.showMap();
+    
+    // Set first market
+    googleMaps.setPosition(coordinates);
+    googleMaps.setTitle('First market');
+    googleMaps.setContents('<div>content1</div>');
+    googleMaps.addMarkersToMap();
+    
+    // Set second market
+    googleMaps.setPosition(coordinates);
+    googleMaps.setTitle('Second market');
+    googleMaps.setContents('<div>content2</div>');
+    googleMaps.addMarkersToMap();
+    
+	
+  }]);
 
-googleMaps.setPosition(currentCoordinates);
-googleMaps.showMap();
-
-// Set first market
-
-googleMaps.setPosition(coordinates);
-googleMaps.setTitle('First market');
-googleMaps.setContents('<div>content1</div>');
-googleMaps.addMarkersToMap();
 
 
-// Set second market
-
-googleMaps.setPosition(coordinates);
-googleMaps.setTitle('Second market');
-googleMaps.setContents('<div>content2</div>');
-googleMaps.addMarkersToMap();
